f7006ba73f756f0a04aecef1bd9ebcd646b318f2
Walmir Monteiro Neto <128555631+Walmir11@users.noreply.github.com>
2024-04-18 20:35:41 -0300

    Adicionando funções em Selenium

--- scr/CursosDio/Selenium/Test.py
+++ scr/CursosDio/Selenium/Test.py
@@ -2,8 +2,26 @@ import time
 from selenium import webdriver
 
 browser = webdriver.Edge()
-
+#Acessa a url
 browser.get('https://google.com')
-time.sleep(5)
+#Maximizar a tela do browser
+browser.maximize_window()
+#Atualiza a página
+browser.refresh()
 
-browser.find_element()
+browser.get('https://www.saucedemo.com/v1/')
+time.sleep(3)
+#volta para página anterior
+browser.back()
+time.sleep(3)
+#Avançar a página
+browser.forward()
+time.sleep(3)
+#Criar uma nova aba
+browser.switch_to.new_window('tab')
+time.sleep(3)
+#Fecha a aba ativa no momento
+browser.close()
+time.sleep(3)
+#Fecha o browser inteiro
+browser.quit() 
