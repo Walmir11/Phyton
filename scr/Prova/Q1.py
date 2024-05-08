3d8561f2404c33faab29153b9f6203cd042badd8
Walmir Monteiro Neto <128555631+Walmir11@users.noreply.github.com>
2024-05-07 23:05:16 -0300

    Fazendo modificações

--- scr/Prova/Q1.py
+++ scr/Prova/Q1.py
@@ -10,6 +10,7 @@ driver = webdriver.Chrome()
 # Acesse a página do Twitch
 driver.maximize_window()
 driver.get('https://www.twitch.tv/')
+nomes_streamers = []
 
 # Faça login (substitua 'seu_usuario' e 'sua_senha' pelas suas credenciais)
 driver.find_element(By.XPATH,"//button[@data-a-target='login-button']").click()
@@ -19,22 +20,28 @@ driver.find_element(By.ID,'password-input').send_keys('monteirobem10homer')
 driver.find_element(By.XPATH,"//button[@data-a-target='passport-login-button']").click()
 time.sleep(8)
 
-# Navegue até as categorias desejadas (por exemplo, Just Chatting)
-driver.get('https://www.twitch.tv/directory/game/Just%20Chatting')
+categorias = ['Just Chatting', 'League of Legends', 'Counter-Strike', 'VALORANT', 'Grand Theft Auto V']
 
-dropdown = Select(driver.find_element(By.XPATH,"//*[@class='Layout-sc-1xcs6mc-0 xxjeD' and text()='Mais espectadores']"))
-dropdown.select_by_visible_text('Mais espectadores')
+for categoria in categorias:
+    driver.get(f'https://www.twitch.tv/directory/game/{categoria}')
+    
+    dropdown = Select(driver.find_element(By.XPATH,"//*[@class='Layout-sc-1xcs6mc-0 xxjeD' and text()='Mais espectadores']"))
+    dropdown.select_by_visible_text('Mais espectadores')
+    
+    # Raspe as informações dos streamers (você precisará ajustar isso conforme necessário)
+    streamers = driver.find_elements(By.XPATH,"//*[@class='tw-image']")
+    for streamer in streamers:
+        nome_streamer = streamer.text
+        nomes_streamers.append({"Nome: ",nome_streamer})
 
-# Raspe as informações dos streamers (você precisará ajustar isso conforme necessário)
-streamers = driver.find_elements(By.XPATH,"//*[@class='tw-image']")
-streamer_data = []
-for streamer in streamers:
-    name = streamer.find_element('.tw-font-size-5.tw-semibold.tw-mg-t-05').text
-    views = streamer.find_element('.tw-font-size-6.tw-semibold.tw-mg-t-05').text
-    streamer_data.append({'Nome': name, 'Visualizações': views})
+    vizualizacao = driver.find_elements(By.XPATH,"//*[@class='ScMediaCardStatWrapper-sc-anph5i-0 jRUNHm tw-media-card-stat']")
+
+    for visualizacoes in vizualizacao:
+        vizualizacao_streamer = visualizacoes.text()
+        nomes_streamers.append({"Vizualizações: ",vizualizacao})    
 
 # Crie um DataFrame com os dados
-df = pd.DataFrame(streamer_data)
+df = pd.DataFrame(nomes_streamers)
 
 # Exiba os 10 streamers com mais visualizações
 top_10_streamers = df.nlargest(10, 'Visualizações')
