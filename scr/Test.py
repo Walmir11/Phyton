a3150c7c267430e3673ac3d8b6ab6af1161ad473
Walmir Monteiro Neto <128555631+Walmir11@users.noreply.github.com>
2024-07-16 21:56:25 -0300

    .

--- scr/Test.py
+++ scr/Test.py
@@ -1,16 +1,9 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
-# Configurações do driver
-chrome_options = Options()
-chrome_options.add_argument('--ignore-certificate-errors')
-
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Edge()
 driver.maximize_window()
 driver.get('https://www.climatempo.com.br')
 driver.implicitly_wait(3)
@@ -27,3 +20,7 @@ time.sleep(2)
 
 driver.find_element(By.XPATH, "(//*[@id='Botao_barra_navegacao_15_dias'])[1]").click()
 time.sleep(2)
+
+driver.find_elements(By.XPATH, '//*[@class="_margin-r-15"]')
+
+//*[@alt='Temperatura mínima']
