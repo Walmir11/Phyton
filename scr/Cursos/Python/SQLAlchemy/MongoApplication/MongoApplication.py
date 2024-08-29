3a3ecc982d91543adcd11e37df152f02856d2750
Walmir11 <walmirneto1990@gmail.com>
2024-08-29 20:50:31 -0300

    Atualizando

--- scr/Cursos/Python/SQLAlchemy/MongoApplication/MongoApplication.py
+++ scr/Cursos/Python/SQLAlchemy/MongoApplication/MongoApplication.py
@@ -1,14 +1,28 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from datetime import datetime
+
+import pymongo as pyM
 
 uri = "mongodb+srv://Walmir11:<db_password>@wcluster.hlrtn.mongodb.net/?retryWrites=true&w=majority&appName=WCluster"
+# crianção do cliente
+client = pyM.MongoClient(uri)
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+# criação do banco de dados
+db = client.test
+# criação da coleção
+collection = db.test_collection
+# inserção de um documento
+print(db.test_collection)
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+# Definição de um documento
+post = {
+    'author' : 'Mike',
+    'text' : 'My first mongodb application based on python',
+    'tags' : ['mongodb', 'python3', 'pymongo'],
+    'date' : datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+}
+
+# Preparação para inserção do documento
+posts = db.posts
+post_id = posts.insert_one(post).inserted_id
+# impressão do id do documento
+print(post_id)
