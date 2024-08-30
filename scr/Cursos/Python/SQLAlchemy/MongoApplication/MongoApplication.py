2547626038d827dbe3a60496eeda77be72d417e1
Walmir11 <walmirneto1990@gmail.com>
2024-08-30 14:43:13 -0300

    Updating

--- scr/Cursos/Python/SQLAlchemy/MongoApplication/MongoApplication.py
+++ scr/Cursos/Python/SQLAlchemy/MongoApplication/MongoApplication.py
@@ -1,8 +1,9 @@
 from datetime import datetime
+from pprint import pprint
 
 import pymongo as pyM
 
-uri = "mongodb+srv://Walmir11:<db_password>@wcluster.hlrtn.mongodb.net/?retryWrites=true&w=majority&appName=WCluster"
+uri = "mongodb+srv://Walmir11:Ginecas10@wcluster.hlrtn.mongodb.net/?retryWrites=true&w=majority&appName=WCluster"
 # crianção do cliente
 client = pyM.MongoClient(uri)
 
@@ -26,3 +27,30 @@ posts = db.posts
 post_id = posts.insert_one(post).inserted_id
 # impressão do id do documento
 print(post_id)
+
+# Listagem de coleções
+print(db.list_collection_names())
+# Mostra o primeiro documento
+#print(db.posts.find_one())
+
+print('*'*50)
+# Mostra todos os documentos de maneira mais organizada
+pprint(posts.find_one())
+
+# bulk inserts
+new_posts = [{'author' : 'Mike',
+              'text' : 'Another post',
+              'tags' : ['bulk', 'insert'],
+              'date' : datetime.now().strftime("%Y-%m-%d %H:%M:%S")},
+             {
+                 'author' : 'Eliot',
+                 'title' : 'MongoDB is fun',
+                 'text' : 'and pretty easy too!',
+                 'date' : datetime.now().strftime("%Y-%m-%d %H:%M:%S")}]
+
+result = posts.insert_many(new_posts)
+print('*'*50)
+# Mostra os ids dos documentos inseridos
+print(result.inserted_ids)
+print('Recuperação final')
+pprint(db.posts.find_one({'author' : 'Eliot'}))
