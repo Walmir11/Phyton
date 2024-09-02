42546392886e0053d099b694b0dcd39ad4b2288d
Walmir11 <walmirneto1990@gmail.com>
2024-09-02 16:34:49 -0300

    Updating

--- scr/Cursos/Python/SQLAlchemy/MongoApplication/MongoApplication.py
+++ scr/Cursos/Python/SQLAlchemy/MongoApplication/MongoApplication.py
@@ -1,16 +1,18 @@
 from datetime import datetime
 from pprint import pprint
-
 import pymongo as pyM
 
 uri = "mongodb+srv://Walmir11:Ginecas10@wcluster.hlrtn.mongodb.net/?retryWrites=true&w=majority&appName=WCluster"
+
 # crianção do cliente
 client = pyM.MongoClient(uri)
 
 # criação do banco de dados
 db = client.test
+
 # criação da coleção
 collection = db.test_collection
+
 # inserção de um documento
 print(db.test_collection)
 
@@ -30,6 +32,7 @@ print(post_id)
 
 # Listagem de coleções
 print(db.list_collection_names())
+
 # Mostra o primeiro documento
 #print(db.posts.find_one())
 
@@ -54,3 +57,9 @@ print('*'*50)
 print(result.inserted_ids)
 print('Recuperação final')
 pprint(db.posts.find_one({'author' : 'Eliot'}))
+
+print('*'*50)
+print('Documentos recuperados na coleção posts')
+# Mostra todos os documentos de maneira mais organizada
+for post in posts.find():
+    pprint(post)
---
+++ scr/Cursos/Python/SQLAlchemy/MongoApplication/SecondFile_PyMongo.py
@@ -0,0 +1,53 @@
+from pprint import pprint
+import pymongo
+import pymongo as pyM
+
+uri = "mongodb+srv://Walmir11:Ginecas10@wcluster.hlrtn.mongodb.net/?retryWrites=true&w=majority&appName=WCluster"
+# crianção do cliente
+client = pyM.MongoClient(uri)
+db = client.test
+posts = db.posts
+
+for post in posts.find():
+    pprint(post)
+
+# Toda vez que é executado o código, um novo documento é inserido
+print('*'*50)
+print('Total de documentos na coleção posts')
+print(posts.count_documents({}))
+
+print('*'*50)
+print('Total de documentos na coleção posts com o autor Mike')
+print(posts.count_documents({'author' : 'Mike'}))
+
+print('*'*50)
+print('Total de documentos na coleção posts com a tag insert')
+print(posts.count_documents({'tags' : 'insert'}))
+
+print('*'*50)
+print('Recuperação de um documento com a tag insert')
+pprint(posts.find_one({'tags' : 'insert'}))
+
+print('*'*50)
+print('Recuperando infos da coleção de forma ordenada pela data')
+for post in posts.find().sort('date'):
+    pprint(post)
+
+print('*'*50)
+# Cria um índice na coleção posts para o campo author de forma ascendente
+result = db.profiles.create_index([('author', pymongo.ASCENDING)], unique=True)
+
+print('Informações sobre os índices da coleção profiles')
+pprint(sorted(list(db.profiles.index_information())))
+
+user_profile = [
+    {'user_id' : 211, 'name' : 'Luke'},
+    {'user_id' : 212, 'name' : 'Solo'}]
+
+# Inserção de vários documentos
+result = db.profiles_user.insert_many(user_profile)
+
+print('*'*50)
+print('Coleções armazenadas no mongoDB\n')
+for x in db.list_collection_names():
+    print(x)
